904e46542b9b1f93bce66d134c0e105d5585577d
Mo <ravrider@protonmail.com>
2025-07-14 20:53:31 +0200

    Update Sidebar.tsx

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -10,7 +10,7 @@ import {
   Menu,
   Circle
 } from 'lucide-react'
-import { Button } from '@/components/ui/Button'
+import { Button } from '@/components/ui/button'
 import { useStore } from '@/stores/useStore'
 import { cn } from '@/lib/utils'
 
